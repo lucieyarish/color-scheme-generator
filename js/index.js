ddbc8817534a77c78640aeeba2ac3d5c8f503928
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-18 07:36:11 +0100

    Wrap all user color selections in a form; prepare dropdown menu with color scheme options

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const colorPicker = document.getElementById('color-picker');
 
 colorPicker.addEventListener('input', function () {
+  //TODO: set colorPicker.value as hex request param
   console.log(colorPicker.value);
 });
