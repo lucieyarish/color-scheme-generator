93b9ca6b994f37ed71e727040b036fe6b9aa9631
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-20 21:11:05 +0100

    Adjust layout; display default color blocks & hex values

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
 const selectionForm = document.getElementById('selection-form');
 const submitBtn = document.getElementById('submit-btn');
+const colorsContainer = document.getElementById('colors-container');
+const colorsList = document.getElementById('colors');
+
+const renderColors = (colors) => {
+  const html = colors
+    .map((color) => {
+      return `
+          <li class="color-container">
+            <div data-id="${color.hex.value}" class="color" style="background-color: ${color.hex.value};"></div>
+            <p class="color-hex">${color.hex.value}</p>
+          </li>
+        `;
+    })
+    .join('');
+
+  colorsList.innerHTML += html;
+};
+
+const url = new URL('https://www.thecolorapi.com/scheme');
+url.searchParams.set('hex', 'C3B1E1');
+url.searchParams.set('mode', 'monochrome');
+url.searchParams.set('count', '5');
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.colors);
+    renderColors(data.colors);
+  });
 
 submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
