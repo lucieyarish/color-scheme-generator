df977fc5a09d308b5ed6e37fc32a473ab72e1f64
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-20 22:02:32 +0100

    Add copy-to-clipboard feature and info dialog

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const selectionForm = document.getElementById('selection-form');
 const submitBtn = document.getElementById('submit-btn');
 const colorsContainer = document.getElementById('colors-container');
 const colorsList = document.getElementById('colors');
+const infoDialog = document.getElementById('info-dialog');
+const dialogCloseBtn = document.getElementById('dialog-close-btn');
 
 const setParams = (url, color, scheme, colorCount) => {
   url.searchParams.set('hex', color);
@@ -21,7 +23,6 @@ const getColors = (selectedColor, selectedScheme, selectedColorCount, e) => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.colors);
       renderColors(data.colors);
     });
 };
@@ -36,7 +37,7 @@ const renderColors = (colors) => {
       return `
           <li class="color-container">
             <div data-id="${color.hex.value}" class="color" style="background-color: ${color.hex.value};"></div>
-            <p class="color-hex">${color.hex.value}</p>
+            <p data-id="${color.hex.value}" class="color-hex">${color.hex.value}</p>
           </li>
         `;
     })
@@ -44,6 +45,19 @@ const renderColors = (colors) => {
 
   colorsList.innerHTML += html;
 };
+
+colorsContainer.addEventListener('click', function (e) {
+  navigator.clipboard.writeText(e.target.dataset.id);
+  document.getElementById(
+    'info-msg'
+  ).textContent = `${e.target.dataset.id} was copied to clipboard!`;
+  infoDialog.inert = true;
+  infoDialog.showModal();
+  infoDialog.inert = false;
+  setTimeout(() => {
+    infoDialog.close();
+  }, 1500);
+});
 
 submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
